77a33ac34861dc2b850b60f148702d26f6f129a4
Cory Hansen <corybyu@gmail.com>
2020-08-06 14:45:08 -0700

    Updated filename convention

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import toDoItem from './toDoItem'
+import ToDoItem from './ to-do-item'
 
 var item1 = new toDoItem("Do the stuff", "Lots of it")
 console.log(item1.title)
---
+++ src/to-do-item.js
@@ -0,0 +1,21 @@
+class ToDoItem {
+    constructor (title, description = "", priority = 3, dueDate = null, tags = []) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority; // From 1 to 5, 5 being highest
+        this.dueDate = dueDate;
+        this.tags = tags;
+    }
+
+    addTag(tag) {
+        this.tags.push(tag);
+    }
+
+    removeTag(tag) {
+        if(this.tags.includes(tag)){
+            this.tags.splice(this.tags.indexOf(tag),1);
+        }
+    }
+}
+
+export default ToDoItem;
