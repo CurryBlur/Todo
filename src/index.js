dd095c7b126f25019b3fbb7de2ac735741a921f7
Cory Hansen <corybyu@gmail.com>
2020-08-06 14:42:51 -0700

    Updated toDoItem to include due date

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import toDoItem from './toDoItem'
+
+var item1 = new toDoItem("Do the stuff", "Lots of it")
+console.log(item1.title)
--- src/toDoItem.js
+++ src/toDoItem.js
@@ -1,18 +1,21 @@
 class toDoItem {
-    constructor (title, description = "", priority = "normal", ) {
-        this.title = title
-        this.description = description
-        this.priority = priority
-        this.tags = []
+    constructor (title, description = "", priority = 3, dueDate = null, tags = []) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority; // From 1 to 5, 5 being highest
+        this.dueDate = dueDate;
+        this.tags = tags;
     }
 
     addTag(tag) {
-        this.tags.push(tag)
+        this.tags.push(tag);
     }
 
     removeTag(tag) {
         if(this.tags.includes(tag)){
-            this.tags.splice(this.tags.indexOf(tag),1)
+            this.tags.splice(this.tags.indexOf(tag),1);
         }
     }
 }
+
+export default toDoItem;
